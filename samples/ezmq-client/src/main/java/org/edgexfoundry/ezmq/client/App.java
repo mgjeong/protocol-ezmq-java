923e1d2312e3260d519639d223f9a3af1a99693e
Jay Sharma <jay.sharma@samsung.com>
2018-08-26 14:19:46 +0530

    Added options for secured mode testing

--- samples/ezmq-client/src/main/java/org/edgexfoundry/ezmq/client/App.java
+++ samples/ezmq-client/src/main/java/org/edgexfoundry/ezmq/client/App.java
@@ -20,8 +20,6 @@ package org.edgexfoundry.ezmq.client;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-
-import org.edgexfoundry.ezmq.domain.core.Event;
 import org.edgexfoundry.ezmq.domain.core.Reading;
 import org.edgexfoundry.ezmq.EZMQAPI;
 import org.edgexfoundry.ezmq.EZMQContentType;
@@ -33,174 +31,208 @@ import org.edgexfoundry.ezmq.EZMQSubscriber.EZMQSubCallback;
 import org.edgexfoundry.ezmq.bytedata.EZMQByteData;
 
 public class App {
-    private static EZMQSubCallback mCallback;
-    // use host name or ip address of publisher on mip when test with docker.
-    private static String mip;
-    private static int mPort;
-    private static EZMQErrorCode result = EZMQErrorCode.EZMQ_ERROR;
-    public static EZMQAPI apiInstance = EZMQAPI.getInstance();
-    public static EZMQSubscriber subInstance = null;
-    public static Lock terminateLock = new ReentrantLock();
-    public static java.util.concurrent.locks.Condition condVar = terminateLock.newCondition();
+  private static EZMQSubCallback mCallback;
+  // use host name or ip address of publisher on mip when test with docker.
+  private static String mip;
+  private static int mPort;
+  private static int mIsSecured;
+  private static EZMQErrorCode result = EZMQErrorCode.EZMQ_ERROR;
+  private static EZMQAPI apiInstance = EZMQAPI.getInstance();
+  private static EZMQSubscriber subInstance = null;
+  private static Lock terminateLock = new ReentrantLock();
+  private static java.util.concurrent.locks.Condition condVar = terminateLock.newCondition();
+  private static String mServerPublicKey = "tXJx&1^QE2g7WCXbF.$$TVP.wCtxwNhR8?iLi&S<";
+  private static String mClientPublicKey = "-QW?Ved(f:<::3d5tJ$[4Er&]6#9yr=vha/caBc(";
+  private static String mClientSecretKey = "ZB1@RS6Kv^zucova$kH(!o>tZCQ.<!Q)6-0aWFmW";
 
-    private static void printEvent(org.edgexfoundry.ezmq.domain.core.Event event) {
-        System.out.println("Device: " + event.getDevice());
-        System.out.println("Readings: ");
-        List<Reading> readings = event.getReadings();
-        for (Reading reading : readings) {
-            System.out.print("Key: " + reading.getName());
-            System.out.println("  Value: " + reading.getValue());
+  private static void printEvent(org.edgexfoundry.ezmq.domain.core.Event event) {
+    System.out.println("Device: " + event.getDevice());
+    System.out.println("Readings: ");
+    List<Reading> readings = event.getReadings();
+    for (Reading reading : readings) {
+      System.out.print("Key: " + reading.getName());
+      System.out.println("  Value: " + reading.getValue());
+    }
+  }
+
+  private static void printByteData(EZMQByteData byteData) {
+    byte[] data = byteData.getByteData();
+    System.out.println("Byte Data Size: " + data.length);
+    for (int i = 0; i < data.length; i++) {
+      System.out.print(" " + data[i]);
+    }
+    System.out.println();
+  }
+
+  private static void callback() {
+    mCallback = new EZMQSubCallback() {
+      public void onMessageCB(EZMQMessage ezmqMsg) {
+        System.out.println("[-------------------------------------");
+        System.out.println("[APP: onMessageCB]");
+        if (null == ezmqMsg) {
+          System.out.println("ezmqMsg is null");
+          return;
         }
+        EZMQContentType type = ezmqMsg.getContentType();
+        System.out.println("Cotent type: " + type);
+
+        if (EZMQContentType.EZMQ_CONTENT_TYPE_PROTOBUF == type) {
+          printEvent((org.edgexfoundry.ezmq.domain.core.Event) ezmqMsg);
+        } else if (EZMQContentType.EZMQ_CONTENT_TYPE_BYTEDATA == type) {
+          printByteData((EZMQByteData) ezmqMsg);
+        }
+        System.out.println("----------------------------------------");
+      }
+
+      public void onMessageCB(String topic, EZMQMessage ezmqMsg) {
+        System.out.println("[-------------------------------------");
+        System.out.println("[APP: onMessageCB]");
+        System.out.println("Topic: " + topic);
+        if (null == ezmqMsg) {
+          System.out.println("ezmqMsg is null");
+          return;
+        }
+        EZMQContentType type = ezmqMsg.getContentType();
+        System.out.println("Cotent type: " + type);
+
+        if (EZMQContentType.EZMQ_CONTENT_TYPE_PROTOBUF == type) {
+          printEvent((org.edgexfoundry.ezmq.domain.core.Event) ezmqMsg);
+        } else if (EZMQContentType.EZMQ_CONTENT_TYPE_BYTEDATA == type) {
+          printByteData((EZMQByteData) ezmqMsg);
+        }
+        System.out.println("----------------------------------------");
+      }
+    };
+  }
+
+  private static void printError() {
+    System.out.println("\nRe-run the application as shown in below examples: ");
+    System.out.println("\n  (1) For subscribing without topic: ");
+    System.out.println(
+        "      java -jar target/edgex-ezmq-subscriber-sample.jar -ip 192.168.0.1 -port 5562");
+    System.out.println("\n  (2) For subscribing without topic: [Secured]");
+    System.out.println(
+        "      java -jar target/edgex-ezmq-subscriber-sample.jar -ip 192.168.0.1 -port 5562 -secured 1");
+    System.out.println("\n  (3) For subscribing with topic: ");
+    System.out.println(
+        "      java -jar target/edgex-ezmq-subscriber-sample.jar -ip 192.168.0.1 -port 5562 -t topic1");
+    System.out.println("\n  (4) For subscribing with topic: [Secured]");
+    System.out.println(
+        "      java -jar target/edgex-ezmq-subscriber-sample.jar -ip 192.168.0.1 -port 5562 -t topic1 -secured 1");
+    System.exit(-1);
+  }
+
+  public static void main(String[] args) {
+
+    // get ip and port and topic from command line arguments
+    if (args.length != 4 && args.length != 6 && args.length != 8) {
+      printError();
     }
 
-    private static void printByteData(EZMQByteData byteData) {
-        byte[] data = byteData.getByteData();
-        System.out.println("Byte Data Size: " + data.length);
-        for (int i = 0; i < data.length; i++) {
-            System.out.print(" " + data[i]);
-        }
-        System.out.println();
+    int n = 0;
+    String topic = null;
+    while (n < args.length) {
+      if (args[n].equalsIgnoreCase("-ip")) {
+        mip = args[n + 1];
+        System.out.println("Given Ip: " + mip);
+        n = n + 2;
+      } else if (args[n].equalsIgnoreCase("-port")) {
+        mPort = Integer.parseInt(args[n + 1]);
+        System.out.println("Given Port: " + mPort);
+        n = n + 2;
+      } else if (args[n].equalsIgnoreCase("-t")) {
+        topic = args[n + 1];
+        System.out.println("Topic is : " + topic);
+        n = n + 2;
+      } else if (args[n].equalsIgnoreCase("-secured")) {
+        mIsSecured = Integer.parseInt(args[n + 1]);
+        System.out.println("Secured: " + mIsSecured);
+        n = n + 2;
+      } else {
+        printError();
+      }
     }
 
-    private static void callback() {
-        mCallback = new EZMQSubCallback() {
-            public void onMessageCB(EZMQMessage ezmqMsg) {
-                System.out.println("[-------------------------------------");
-                System.out.println("[APP: onMessageCB]");
-                if (null == ezmqMsg) {
-                    System.out.println("ezmqMsg is null");
-                    return;
-                }
-                EZMQContentType type = ezmqMsg.getContentType();
-                System.out.println("Cotent type: " + type);
-
-                if (EZMQContentType.EZMQ_CONTENT_TYPE_PROTOBUF == type) {
-                    printEvent((org.edgexfoundry.ezmq.domain.core.Event) ezmqMsg);
-                } else if (EZMQContentType.EZMQ_CONTENT_TYPE_BYTEDATA == type) {
-                    printByteData((EZMQByteData) ezmqMsg);
-                }
-                System.out.println("----------------------------------------");
-            }
-
-            public void onMessageCB(String topic, EZMQMessage ezmqMsg) {
-                System.out.println("[-------------------------------------");
-                System.out.println("[APP: onMessageCB]");
-                System.out.println("Topic: " + topic);
-                if (null == ezmqMsg) {
-                    System.out.println("ezmqMsg is null");
-                    return;
-                }
-                EZMQContentType type = ezmqMsg.getContentType();
-                System.out.println("Cotent type: " + type);
-
-                if (EZMQContentType.EZMQ_CONTENT_TYPE_PROTOBUF == type) {
-                    printEvent((org.edgexfoundry.ezmq.domain.core.Event) ezmqMsg);
-                } else if (EZMQContentType.EZMQ_CONTENT_TYPE_BYTEDATA == type) {
-                    printByteData((EZMQByteData) ezmqMsg);
-                }
-                System.out.println("----------------------------------------");
-            }
-        };
+    apiInstance.initialize();
+    callback();
+    subInstance = new EZMQSubscriber(mip, mPort, mCallback);
+    if (1 == mIsSecured) {
+      try {
+        result = subInstance.setServerPublicKey(mServerPublicKey);
+      } catch (Exception e) {
+        System.out.println("setServerPublicKey exception: " + e.getMessage());
+        e.printStackTrace();
+        return;
+      }
+      if (result != EZMQErrorCode.EZMQ_OK) {
+        System.out.println("Set server public key API: error occured");
+        return;
+      }
+      try {
+        result = subInstance.setClientKeys(mClientSecretKey, mClientPublicKey);
+      } catch (Exception e) {
+        System.out.println("setClientKeys exception: " + e.getMessage());
+        e.printStackTrace();
+        return;
+      }
+      if (result != EZMQErrorCode.EZMQ_OK) {
+        System.out.println("Set server client keys API: error occured");
+        return;
+      }
+    }
+    result = subInstance.start();
+    if (result != EZMQErrorCode.EZMQ_OK) {
+      System.out.println("start API: error occured");
+      return;
     }
 
-    private static void printError() {
-        System.out.println("\nRe-run the application as shown in below examples: ");
-        System.out.println("\n  (1) For subscribing without topic: ");
-        System.out.println(
-                "      java -jar target/edgex-ezmq-subscriber-sample.jar -ip 107.108.81.116 -port 5562");
-        System.out.println(
-                "      java -jar target/edgex-ezmq-subscriber-sample.jar -ip localhost -port 5562");
-        System.out.println("\n  (2) For subscribing with topic: ");
-        System.out.println(
-                "      java -jar target/edgex-ezmq-subscriber-sample.jar -ip 107.108.81.116 -port 5562 -t topic1");
-        System.out.println(
-                "      java -jar target/edgex-ezmq-subscriber-sample.jar -ip localhost -port 5562 -t topic1");
-        System.exit(-1);
-    }
-
-    public static void main(String[] args) {
-
-        // get ip and port and topic from command line arguments
-        if (args.length != 4 && args.length != 6) {
-            printError();
-        }
-
-        int n = 0;
-        String topic = null;
-        while (n < args.length) {
-            if (args[n].equalsIgnoreCase("-ip")) {
-                mip = args[n + 1];
-                System.out.println("Given Ip: " + mip);
-                n = n + 2;
-            } else if (args[n].equalsIgnoreCase("-port")) {
-                mPort = Integer.parseInt(args[n + 1]);
-                System.out.println("Given Port: " + mPort);
-                n = n + 2;
-            } else if (args[n].equalsIgnoreCase("-t")) {
-                topic = args[n + 1];
-                System.out.println("Topic is : " + topic);
-                n = n + 2;
-            } else {
-                printError();
-            }
-        }
-
-        apiInstance.initialize();
-        callback();
-        subInstance = new EZMQSubscriber(mip, mPort, mCallback);
-        result = subInstance.start();
-        if (result != EZMQErrorCode.EZMQ_OK) {
-            System.out.println("start API: error occured");
-            return;
-        }
-
-        // handle command line ctrl+c signal, stop subscriber and signal main
-        // program to exit
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            public void run() {
-                try {
-                    // stopping the subscriber
-                    result = subInstance.stop();
-                    if (result != EZMQErrorCode.EZMQ_OK) {
-                        System.out.println("Stop API: error occured");
-                    }
-                    result = apiInstance.terminate();
-                    if (result != EZMQErrorCode.EZMQ_OK) {
-                        System.out.println("Terminate API: error occured");
-                    }
-                    terminateLock.lock();
-                    condVar.signalAll();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    terminateLock.unlock();
-                }
-            }
-        }));
-
-        if (null == topic) {
-            result = subInstance.subscribe();
-        } else {
-            result = subInstance.subscribe(topic);
-        }
-
-        if (result != EZMQErrorCode.EZMQ_OK) {
-            System.out.println("subscribe API: error occured");
-            return;
-        }
-
-        System.out.println("Suscribed to publisher.. -- Waiting for Events --");
-
-        // Prevent main thread from exit
+    // handle command line ctrl+c signal, stop subscriber and signal main
+    // program to exit
+    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+      public void run() {
         try {
-            terminateLock.lock();
-            while(apiInstance.getStatus() != EZMQStatusCode.EZMQ_Terminated) {
-                condVar.await();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+          // stopping the subscriber
+          result = subInstance.stop();
+          if (result != EZMQErrorCode.EZMQ_OK) {
+            System.out.println("Stop API: error occured");
+          }
+          result = apiInstance.terminate();
+          if (result != EZMQErrorCode.EZMQ_OK) {
+            System.out.println("Terminate API: error occured");
+          }
+          terminateLock.lock();
+          condVar.signalAll();
+        } catch (Exception e) {
+          e.printStackTrace();
         } finally {
-            terminateLock.unlock();
+          terminateLock.unlock();
         }
+      }
+    }));
+
+    if (null == topic) {
+      result = subInstance.subscribe();
+    } else {
+      result = subInstance.subscribe(topic);
     }
+
+    if (result != EZMQErrorCode.EZMQ_OK) {
+      System.out.println("subscribe API: error occured");
+      return;
+    }
+
+    System.out.println("Suscribed to publisher.. -- Waiting for Events --");
+
+    // Prevent main thread from exit
+    try {
+      terminateLock.lock();
+      while (apiInstance.getStatus() != EZMQStatusCode.EZMQ_Terminated) {
+        condVar.await();
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      terminateLock.unlock();
+    }
+  }
 }
--- samples/ezmq-server/src/main/java/org/edgexfoundry/ezmq/server/App.java
+++ samples/ezmq-server/src/main/java/org/edgexfoundry/ezmq/server/App.java
@@ -21,7 +21,6 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
 import org.edgexfoundry.ezmq.domain.core.Event;
 import org.edgexfoundry.ezmq.domain.core.Reading;
 import org.edgexfoundry.ezmq.EZMQAPI;
@@ -31,154 +30,174 @@ import org.edgexfoundry.ezmq.EZMQPublisher;
 import org.edgexfoundry.ezmq.bytedata.EZMQByteData;
 
 public class App {
-    private static EZMQCallback mCallback;
-    private static int mPort;
-    private static EZMQErrorCode result = EZMQErrorCode.EZMQ_ERROR;
-    public static EZMQAPI apiInstance = EZMQAPI.getInstance();
-    public static EZMQPublisher pubInstance = null;
+  private static EZMQCallback mCallback;
+  private static int mPort;
+  private static int mIsSecured;
+  private static EZMQErrorCode result = EZMQErrorCode.EZMQ_ERROR;
+  public static EZMQAPI apiInstance = EZMQAPI.getInstance();
+  public static EZMQPublisher pubInstance = null;
+  public static String mServerSecretKey = "[:X%Q3UfY+kv2A^.wv:(qy2E=bk0L][cm=mS3Hcx";
 
-    private static void callback() {
-        mCallback = new EZMQCallback() {
+  private static void callback() {
+    mCallback = new EZMQCallback() {
 
-            @Override
-            public void onStopCB(EZMQErrorCode code) {
-            }
+      @Override
+      public void onStopCB(EZMQErrorCode code) {}
 
-            @Override
-            public void onStartCB(EZMQErrorCode code) {
-            }
+      @Override
+      public void onStartCB(EZMQErrorCode code) {}
 
-            @Override
-            public void onErrorCB(EZMQErrorCode code) {
-            }
-        };
+      @Override
+      public void onErrorCB(EZMQErrorCode code) {}
+    };
+  }
+
+  public static org.edgexfoundry.ezmq.domain.core.Event getEdgeXEvent() {
+
+    List<Reading> readings = null;
+    readings = new ArrayList<Reading>();
+    Random rand = new Random();
+    Reading reading1 = new Reading();
+    reading1.setName("TestReading1");
+    reading1.setValue(rand.nextLong() + "");
+    reading1.setCreated(0);
+    reading1.setDevice("TestDevice1");
+    reading1.setModified(10);
+    reading1.setId("id1");
+    reading1.setOrigin(15);
+    reading1.setPushed(20);
+
+    Reading reading2 = new Reading();
+    reading2.setName("TestReading2");
+    reading2.setValue(rand.nextLong() + "");
+    reading2.setCreated(25);
+    reading2.setDevice("TestDevice2");
+    reading2.setModified(30);
+    reading2.setId("id2");
+    reading2.setOrigin(35);
+    reading2.setPushed(30);
+
+    readings.add(reading1);
+    readings.add(reading2);
+
+    org.edgexfoundry.ezmq.domain.core.Event event = new Event("Test", readings);
+    event.setCreated(10);
+    event.setModified(20);
+    event.setId("id");
+    event.markPushed(new Timestamp(System.currentTimeMillis()).getTime());
+    event.setOrigin(new Timestamp(System.currentTimeMillis()).getTime());
+
+    return event;
+  }
+
+  private static void printError() {
+    System.out.println("\nRe-run the application as shown in below examples: ");
+    System.out.println("\n  (1) For publishing without topic: ");
+    System.out.println("      java -jar target/edgex-ezmq-publisher-sample.jar -port 5562");
+    System.out.println("\n  (2) For publishing without topic: [Secured]");
+    System.out
+        .println("      java -jar target/edgex-ezmq-publisher-sample.jar -port 5562 -secured 1");
+    System.out.println("\n  (3) For publishing with topic: ");
+    System.out
+        .println("      java -jar target/edgex-ezmq-publisher-sample.jar -port 5562 -t topic1");
+    System.out.println("\n  (4) For publishing with topic: [Secured]");
+    System.out.println(
+        "      java -jar target/edgex-ezmq-publisher-sample.jar -port 5562 -t topic1 -secured 1");
+    System.exit(-1);
+  }
+
+  public static EZMQByteData getEzmqByteData() {
+    byte[] byteData = {0x40, 0x05, 0x10, 0x11, 0x12};
+    EZMQByteData data = new EZMQByteData(byteData);
+    return data;
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    // get port and topic from command line arguments
+    int n = 0;
+    String topic = null;
+    if (args.length != 2 && args.length != 4 && args.length != 6) {
+      printError();
+    }
+    while (n < args.length) {
+      if (args[n].equalsIgnoreCase("-port")) {
+        mPort = Integer.parseInt(args[n + 1]);
+        System.out.println("Given Port: " + mPort);
+        n = n + 2;
+      } else if (args[n].equalsIgnoreCase("-t")) {
+        topic = args[n + 1];
+        System.out.println("Topic is : " + topic);
+        n = n + 2;
+      } else if (args[n].equalsIgnoreCase("-secured")) {
+        mIsSecured = Integer.parseInt(args[n + 1]);
+        System.out.println("Secured: " + mIsSecured);
+        n = n + 2;
+      } else {
+        printError();
+      }
     }
 
-    public static org.edgexfoundry.ezmq.domain.core.Event getEdgeXEvent() {
-
-        List<Reading> readings = null;
-        readings = new ArrayList<Reading>();
-        Random rand = new Random();
-        Reading reading1 = new Reading();
-        reading1.setName("TestReading1");
-        reading1.setValue(rand.nextLong() + "");
-        reading1.setCreated(0);
-        reading1.setDevice("TestDevice1");
-        reading1.setModified(10);
-        reading1.setId("id1");
-        reading1.setOrigin(15);
-        reading1.setPushed(20);
-
-        Reading reading2 = new Reading();
-        reading2.setName("TestReading2");
-        reading2.setValue(rand.nextLong() + "");
-        reading2.setCreated(25);
-        reading2.setDevice("TestDevice2");
-        reading2.setModified(30);
-        reading2.setId("id2");
-        reading2.setOrigin(35);
-        reading2.setPushed(30);
-
-        readings.add(reading1);
-        readings.add(reading2);
-
-        org.edgexfoundry.ezmq.domain.core.Event event = new Event("Test", readings);
-        event.setCreated(10);
-        event.setModified(20);
-        event.setId("id");
-        event.markPushed(new Timestamp(System.currentTimeMillis()).getTime());
-        event.setOrigin(new Timestamp(System.currentTimeMillis()).getTime());
-
-        return event;
+    apiInstance.initialize();
+    callback();
+    pubInstance = new EZMQPublisher(mPort, mCallback);
+    if (1 == mIsSecured) {
+      try {
+        result = pubInstance.setServerPrivateKey(mServerSecretKey);
+      } catch (Exception e) {
+        System.out.println("setServerPrivateKey exception: " + e.getMessage());
+        e.printStackTrace();
+      }
+      if (result != EZMQErrorCode.EZMQ_OK) {
+        System.out.println("Set server private key API: error occured");
+        return;
+      }
+    }
+    result = pubInstance.start();
+    if (result != EZMQErrorCode.EZMQ_OK) {
+      System.out.println("start API: error occured");
+      return;
     }
 
-    private static void printError() {
-        System.out.println("\nRe-run the application as shown in below examples: ");
-        System.out.println("\n  (1) For publishing without topic: ");
-        System.out.println("      java -jar target/edgex-ezmq-publisher-sample.jar -port 5562");
-        System.out.println("\n  (2) For publishing with topic: ");
-        System.out.println(
-                "      java -jar target/edgex-ezmq-publisher-sample.jar -port 5562 -t topic1");
-        System.exit(-1);
-    }
-
-    public static EZMQByteData getEzmqByteData() {
-        byte[] byteData = { 0x40, 0x05, 0x10, 0x11, 0x12 };
-        EZMQByteData data = new EZMQByteData(byteData);
-        return data;
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-
-        // get port and topic from command line arguments
-        int n = 0;
-        String topic = null;
-        if (args.length != 2 && args.length != 4) {
-            printError();
-        }
-        while (n < args.length) {
-            if (args[n].equalsIgnoreCase("-port")) {
-                mPort = Integer.parseInt(args[n + 1]);
-                System.out.println("Given Port: " + mPort);
-                n = n + 2;
-            } else if (args[n].equalsIgnoreCase("-t")) {
-                topic = args[n + 1];
-                System.out.println("Topic is : " + topic);
-                n = n + 2;
-            } else {
-                printError();
-            }
-        }
-
-        apiInstance.initialize();
-        callback();
-        pubInstance = new EZMQPublisher(mPort, mCallback);
-        result = pubInstance.start();
+    // handle command line ctrl+c signal and call stop publisher
+    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+      public void run() {
+        result = pubInstance.stop();
         if (result != EZMQErrorCode.EZMQ_OK) {
-            System.out.println("start API: error occured");
-            return;
+          System.out.println("stop API: error occured");
         }
-
-        // handle command line ctrl+c signal and call stop publisher
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            public void run() {
-                result = pubInstance.stop();
-                if (result != EZMQErrorCode.EZMQ_OK) {
-                    System.out.println("stop API: error occured");
-                }
-                result = apiInstance.terminate();
-                if (result != EZMQErrorCode.EZMQ_OK) {
-                    System.out.println("Terminate API: error occured");
-                }
-                System.out.println("!!!!! Exiting !!!!");
-            }
-        }));
-
-        // This delay is added to prevent JeroMQ first packet drop during
-        // initial connection of publisher and subscriber.
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+        result = apiInstance.terminate();
+        if (result != EZMQErrorCode.EZMQ_OK) {
+          System.out.println("Terminate API: error occured");
         }
+        System.out.println("!!!!! Exiting !!!!");
+      }
+    }));
 
-        org.edgexfoundry.ezmq.domain.core.Event event = getEdgeXEvent();
-        int i = 1;
-        System.out.println("--------- Will Publish 15 events at interval of 2 seconds ---------");
-        while (i <= 15) {
-            if (null == topic) {
-                result = pubInstance.publish(event);
-            } else {
-                result = pubInstance.publish(topic, event);
-            }
-            if (result != EZMQErrorCode.EZMQ_OK) {
-                System.out.println("publish API: error occured");
-                return;
-            }
-            System.out.println("Event " + i + " Published!");
-            Thread.sleep(2000);
-            i++;
-        }
+    // This delay is added to prevent JeroMQ first packet drop during
+    // initial connection of publisher and subscriber.
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e1) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
     }
+
+    org.edgexfoundry.ezmq.domain.core.Event event = getEdgeXEvent();
+    int i = 1;
+    System.out.println("--------- Will Publish 15 events at interval of 2 seconds ---------");
+    while (i <= 15) {
+      if (null == topic) {
+        result = pubInstance.publish(event);
+      } else {
+        result = pubInstance.publish(topic, event);
+      }
+      if (result != EZMQErrorCode.EZMQ_OK) {
+        System.out.println("publish API: error occured");
+        return;
+      }
+      System.out.println("Event " + i + " Published!");
+      Thread.sleep(2000);
+      i++;
+    }
+  }
 }
